595782918359cf95a96611d3008ee888c4e39da0
bpikekh <brooks@kelahealth.com>
2023-08-21 15:10:07 -0500

    add cpt - fix postgres rc

--- migrations/sqls/1-procedure-mappings.sql
+++ migrations/sqls/1-procedure-mappings.sql
@@ -2,5 +2,6 @@ CREATE TABLE procedure_mapping (
   id serial PRIMARY KEY,
   nicename varchar(64) not null,
   identifier varchar(64) unique not null,
-  icd_10_partial varchar(24)
+  icd_10_partials varchar(24),
+  cpts varchar(24)
 );
